LineageOS/android_packages_apps_FMRadio

Disable libfmjni on QC hardware
QCOM has its own implementation elsewhere

Change-Id: Ie810369453e9a4c8f6c65b40bec6eef73dbe75fc

jni/fmr/Android.mk
@@ -31,5 +31,7 @@ LOCAL_SHARED_LIBRARIES := \
     libmedia \
 
 LOCAL_MODULE := libfmjni
-include $(BUILD_SHARED_LIBRARY)
 
+ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
+include $(BUILD_SHARED_LIBRARY)
+endif

sonyxperiadev/packages-apps-FMRadio

jni/fmr/Android.mk
@@ -32,5 +32,7 @@ LOCAL_SHARED_LIBRARIES := \
     libmedia \
 
 LOCAL_MODULE := libfmjni
-include $(BUILD_SHARED_LIBRARY)
 
+ifneq ($(BOARD_HAVE_QCOM_FM),true)
+include $(BUILD_SHARED_LIBRARY)
+endif