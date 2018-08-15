beaa95febe80d94d8ad1973882905d38fad3e04a
Zoran Jovanovic <zoran.jovanovic@sony.com>
2018-08-15 15:15:49 +0200

    Starting API 28 private APIs must be declared explicitly
    Signed-off-by: Zoran Jovanovic <zoran.jovanovic@sony.com>

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
-
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := FMRadio
